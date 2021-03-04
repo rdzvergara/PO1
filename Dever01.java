ee3e98b3036200379c7edbe75a5a6e825ad1f9d8
rdzvergara <rodrigo.vergara@soulasalle.com.br>
2021-03-04 20:12:46 -0300

    Add files via upload

---
+++ Dever01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Dever01 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int x;
+        x= input.nextInt();
+        for(int i = 0; i <= 10; i++){
+            System.out.println(x+i);
+
+        }
+    }
+}
